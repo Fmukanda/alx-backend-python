ef7374828e9baf29ed673134fdb2ca03787d733c
Fernandes Mukanda <fernandezmukanda@gmail.com>
2025-09-20 16:25:36 +0300

    Update test_utils.py

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,1 +1,25 @@
+#!/usr/bin/env python3
+"""
+Unit tests for the utils.access_nested_map function.
+"""
+import unittest
+from parameterized import parameterized
+from utils import access_nested_map
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """
+    Test class for the access_nested_map function.
+    """
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2),
+    ])
+    def test_access_nested_map(self, nested_map, path, expected_output):
+        """
+        Test that access_nested_map returns the expected value.
+        """
+        result = access_nested_map(nested_map, path)
+        self.assertEqual(result, expected_output)
 
