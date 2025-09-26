39ba05d6560896763c20745211fa0e6af05b6426
Fernandes Mukanda <fernandezmukanda@gmail.com>
2025-09-26 17:03:43 +0300

    Update urls.py

--- messaging_app/messaging_app/urls.py
+++ messaging_app/messaging_app/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for alx_travel_app project.
+URL configuration for messaging_app project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
@@ -20,3 +20,4 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
