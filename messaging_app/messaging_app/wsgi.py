a258921b56a255e0cd029de65d58dc89e75476d6
Fernandes Mukanda <fernandezmukanda@gmail.com>
2025-09-26 16:22:05 +0300

    Update wsgi.py

--- messaging_app/messaging_app/wsgi.py
+++ messaging_app/messaging_app/wsgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'alx_travel_app.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'messaging_app.settings')
 
 application = get_wsgi_application()
+
