587d47bff98b4ccc1dca3b0a055be8f5932da6b5
Fernandes Mukanda <fernandezmukanda@gmail.com>
2025-09-26 16:16:51 +0300

    Update urls.py

--- messaging_app/messaging_app/chats/urls.py
+++ messaging_app/messaging_app/chats/urls.py
@@ -2,6 +2,7 @@ rom django.urls import path
 from . import views
 
 urlpatterns = [
-    path('listings/', views.ListingList.as_view(), name='listing-list'),
-    path('listings/<int:pk>/', views.ListingDetail.as_view(), name='listing-detail'),
+    path('chatings/', views.ChatingChat.as_view(), name='listing-list'),
+    path('chatings/<int:pk>/', views.ChatingDetail.as_view(), name='listing-detail'),
+
 ]
